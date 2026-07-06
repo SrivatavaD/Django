6508976d775d2aec8342aff2d6b17e12c1a9651e
SrivastavaD <sdevansh296@gmail.com>
2026-07-06 15:22:44 +0530

    Views and URL in Django

--- DayOne/DayOne/DayOne/urls.py
+++ DayOne/DayOne/DayOne/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app1 import views
+from app1.views import learn_django
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
-    path('myfun/',views.myfunction,name='myfunction')
-    
+    path('myfun/',views.myfunction,name='myfunction'),
+    path('dj/',learn_django),
+    path('py/',learn_django,{'Status':'ok'}),
+
 ]
