2cfb7edfa7b492491428814e2ddeec2b53b74de4
SrivastavaD <sdevansh296@gmail.com>
2026-07-14 23:45:12 +0530

    Django Templete Inheritance

--- DayFour/Template/course/urls.py
+++ DayFour/Template/course/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from core.views import home
+from course.views import learn_django, learn_python
 
 
 urlpatterns = [
     path('',home),
+    path('dj/' , learn_django),
+    path('py/' , learn_python),
 ]
