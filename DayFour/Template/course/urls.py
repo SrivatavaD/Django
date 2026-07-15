b1fd9b0f855899154b206472830f77f149af2d0b
SrivastavaD <sdevansh296@gmail.com>
2026-07-15 23:54:14 +0530

    Creating Hyperlink in Django

--- DayFour/Template/course/urls.py
+++ DayFour/Template/course/urls.py
@@ -1,11 +1,8 @@
-from django.contrib import admin
 from django.urls import path
-from core.views import home
 from course.views import learn_django, learn_python
 
 
 urlpatterns = [
-    path('',home),
-    path('dj/' , learn_django),
-    path('py/' , learn_python),
+    path('dj/', learn_django, name='learn_django'),
+    path('py/', learn_python, name='learn_python'),
 ]
